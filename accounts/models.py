bc46e162d37dba10a177033277d21a341ff39724
Srijan-SS02 <codeofficialsrijansriv@gmail.com>
2022-12-03 23:51:37 +0530

    Something in progress/ missed track/ completing OOPS

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
         return self.name
 
 
+class tags(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+ 
 class Product(models.Model):
     CATEGORY=(
         ('Indoor','Indoor'),
@@ -21,18 +28,24 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags= models.ManyToManyField(tags)
 
+    def __str__(self) -> str:
+        return self.name
+    
 
 class Order(models.Model):
     STATUS=(
         ('Pending', 'Pending'),
         ('Out for Delivery','Out for Delivery'),
         ('Delivered', 'Delivered'),
-    )
+    ) 
 
-    # customer=
-    # product=
+    customer= models.ForeignKey(Customer,null = True, on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null = True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
+    
