1ff08a41f60dfc2287a4d8ae02e563cc72748a5e
Srijan-SS02 <codeofficialsrijansriv@gmail.com>
2022-12-31 01:54:30 +0530

    firse bhool gaya commit karna time pe :(

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm 
 
 # Create your views here.
 
@@ -23,6 +24,7 @@ def home(request):
 
 def products(request):
 	products = Product.objects.all() 
+
 	return render(request, 'accounts/products.html', {
 		"products" : products
 
@@ -34,9 +36,56 @@ def customer(request, pk_test):
 	customer=Customer.objects.get(id=pk_test)
 	orders= customer.order_set.all()
 	order_count=orders.count()
+
 	return render(request, 'accounts/customer.html' ,{
 		"customer":customer,
 		"orders" : orders, 
 		"order_count": order_count,
 
 	})
+
+
+def createOrder(request):
+	
+	form = OrderForm
+	if request.method == 'POST':
+		form = OrderForm(request.POST)
+		if form.is_valid():
+			form.save() 
+			return redirect('/')  
+
+
+	return render(request, 'accounts/order_forms.html',{
+		'form' : form,
+	})
+
+
+def updateOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	form= OrderForm(instance=order)    ##############
+
+	if request.method == 'POST':
+		form = OrderForm(request.POST, instance=order)  ###############
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	return render(request, 'accounts/order_forms.html', {
+		'form':form
+	})
+
+
+
+def deleteOrder(request, pk):
+
+	# order = Order.objects.get(id=pk)
+	# form= OrderForm(instance=order)
+
+	# if request.method == 'POST':
+	# 	form = OrderForm(request.POST, instance=order)
+	# 	if form.is_valid():
+	# 		...
+		
+
+		return render(request, 'accounts/delete.html')
