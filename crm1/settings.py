44c9f2bcd188715dfbbdf6e6b95917bac4ea26fb
Srijan-SS02 <codeofficialsrijansriv@gmail.com>
2023-01-25 01:13:29 +0530

    Working on Postgress conection

--- crm1/settings.py
+++ crm1/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
